850d064ef367b140f72036cfa338213d7c7445a1
Germán Oñate <o.germanmartin@gmail.com>
2023-09-20 11:33:30 +0200

    updating commands

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/witaas/",
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      react(),
+    ],
+    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+    base: './'
+  }
+});
